1ae140ef63b01343395796c69d5cc0ddd354648a
Killian <c15490862@mydit.ie>
2017-10-04 15:52:03 +0000

    Changes

--- app.py
+++ app.py
@@ -17,6 +17,9 @@ mysql.init_app(app)
 def hello(): # Name of the method
     cur = mysql.connection.cursor() #create a connection to the SQL instance
     cur.execute('''SELECT * FROM students''') # execute an SQL statment
+    cur.execute('''INSERT INTO students (studentName,email) values ("Student1","Student1@mydit.ie")''' ) #add a student record  
+    cur.execute('''update student set studentName = 'Updated Name' WHERE studentName="Student1" ''') # update student record
+    cur.execute('''DELETE FROM student  WHERE studentName = "Updated Name" ''') # delete from table
     rv = cur.fetchall() #Retreive all rows returend by the SQL statment
     return str(rv)      #Return the data in a string format
 if __name__ == "__main__":
